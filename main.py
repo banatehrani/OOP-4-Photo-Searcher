a196b8c337553f11628b71fd1746eeefc6181f18
banatehrani <banatehrani@gmail.com>
2021-10-24 06:46:45 -0400

    Functions split into more

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    def search_image(self):
+    def get_image_link(self):
         # Get user query from TextInput
         query = self.manager.current_screen.ids.user_query.text
         # Get wikipedia page and the link for the first image
         page = wikipedia.page(query)
-        image_link = page.images[0]
-        # Download the image
+        return page.images[0]
+
+    def download_image(self):
+        # Set the header to be able to download the image
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         }
-        req = requests.get(image_link, headers=headers)
+        # Download the image
+        req = requests.get(self.get_image_link(), headers=headers)
         imagepath = 'files/image.jpg'
         with open(imagepath, 'wb') as file:
             file.write(req.content)
+        return imagepath
+
+    def set_image(self):
         # Set the image in the Image widget
-        self.manager.current_screen.ids.img.source = imagepath
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
